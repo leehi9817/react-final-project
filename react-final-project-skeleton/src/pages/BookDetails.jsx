16f51f293c81a15c09550347f6a96cad31581c17
leehi9817 <leehi9817@gmail.com>
2026-06-15 17:48:40 +0900

    feat: add book details page

--- react-final-project-skeleton/src/pages/BookDetails.jsx
+++ react-final-project-skeleton/src/pages/BookDetails.jsx
@@ -1,7 +1,15 @@
-const BookDetails = () => {
-  return (
-    <></>
-  )
-}
+import BookDetail from "../components/BookDetail";
+import { useParams } from "react-router-dom";
 
-export default BookDetails
+const BookDetails = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>Book Details</h1>
+      <BookDetail bookId={id} />
+    </div>
+  );
+};
+
+export default BookDetails;
