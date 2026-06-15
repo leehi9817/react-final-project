8a6bc309d30a63b26eb8403d0b7b8ef6413934f2
leehi9817 <leehi9817@gmail.com>
2026-06-15 14:56:35 +0900

    feat: add book list page layout

--- react-final-project-skeleton/src/components/BookList.jsx
+++ react-final-project-skeleton/src/components/BookList.jsx
@@ -1,7 +1,17 @@
-const BookList = () => {
-  return (
-    <></>
-  )
-}
+import { Link } from "react-router-dom";
+import { useBookContext } from "../context/BookContext";
+import Loading from "./Loading";
+import Error from "./Error";
+import { renderStars, getBookEmoji } from "../utils";
+import styles from "./BookList.module.css";
 
-export default BookList
+const BookList = () => {
+  const { books, dispatch, loading, error } = useBookContext();
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+
+  return <section className={styles.list}></section>;
+};
+
+export default BookList;
