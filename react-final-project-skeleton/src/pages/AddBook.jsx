689e169420f8deb6fab0acf3a94b271ce0f04edf
leehi9817 <leehi9817@gmail.com>
2026-06-16 12:29:01 +0900

    feat: add handleAddBook feature to the add book page

--- react-final-project-skeleton/src/pages/AddBook.jsx
+++ react-final-project-skeleton/src/pages/AddBook.jsx
@@ -1,12 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import { useBookContext } from "../contexts/BookContext";
+import { useBookContext } from "../context/BookContext";
 import BookForm from "../components/BookForm";
 
 const AddBook = () => {
   const navigate = useNavigate();
   const { dispatch } = useBookContext();
 
-  const handleAddBook = async (newBook) => {};
+  const handleAddBook = async (newBook) => {
+    try {
+      const response = await fetch("http://localhost:3000/books", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newBook),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to add book");
+      }
+
+      const data = await response.json();
+      dispatch({ type: "ADD_BOOK", payload: data });
+      navigate("/");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   return (
     <div>
