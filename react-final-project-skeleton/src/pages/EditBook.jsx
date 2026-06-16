d4e04f9e0f7956d24c134640289267a277f74e18
leehi9817 <leehi9817@gmail.com>
2026-06-16 16:06:52 +0900

    feat: set form data to the edit book page

--- react-final-project-skeleton/src/pages/EditBook.jsx
+++ react-final-project-skeleton/src/pages/EditBook.jsx
@@ -1,7 +1,38 @@
-const EditBook = () => {
-  return (
-    <></>
-  )
-}
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useBookContext } from "../context/BookContext";
+import Loading from "../components/Loading";
+import BookForm from "../components/BookForm";
 
-export default EditBook
+const EditBook = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { dispatch } = useBookContext();
+  const [bookData, setBookData] = useState(null);
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      const response = await fetch(`http://localhost:3000/books/${id}`);
+      if (response.ok) {
+        const data = await response.json();
+        setBookData(data);
+      }
+    };
+    fetchBook();
+  }, [id]);
+
+  const handleUpdateBook = async (updatedBook) => {};
+
+  if (!bookData) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <h1>Edit Book</h1>
+      <BookForm initialData={bookData} onSubmit={handleUpdateBook} />
+    </div>
+  );
+};
+
+export default EditBook;
